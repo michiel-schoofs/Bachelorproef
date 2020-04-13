2815df77a8db69ef007f8e983b7f67afe6a9bfd3
Michiel Schoofs <michiel@starringjane.com>
2020-04-13 16:41:12 +0200

    [feat] added small demo

---
+++ Demo/ipfs/IPFS test/Program.cs
@@ -0,0 +1,17 @@
+﻿using Ipfs;
+using Ipfs.Http;
+using System;
+using System.Linq;
+
+namespace IPFS_test {
+    class Program {
+        static void Main(string[] args) {
+            //Makes a new IPFS node
+            IpfsClient client = new IpfsClient();
+            //add image to node
+            IFileSystemNode node = client.FileSystem.AddFileAsync("images/index.jpg").Result;
+            //show info about image
+            Console.WriteLine($"The CID of this node is: {node.Id} and it contains {node.Links.Count()} links to other files");
+        }
+    }
+}
