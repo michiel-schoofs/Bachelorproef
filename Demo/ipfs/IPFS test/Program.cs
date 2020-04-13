97be6158b86c4522d8316f0117599fc3a50b4cec
Michiel Schoofs <michiel@starringjane.com>
2020-04-13 18:30:18 +0200

    [feat] added CID demo

--- Demo/ipfs/IPFS test/Program.cs
+++ Demo/ipfs/IPFS test/Program.cs
@@ -2,6 +2,7 @@
 using Ipfs.Http;
 using System;
 using System.Linq;
+using IPFS_test.CID;
 
 namespace IPFS_test {
     class Program {
@@ -12,6 +13,12 @@ namespace IPFS_test {
             IFileSystemNode node = client.FileSystem.AddFileAsync("images/index.jpg").Result;
             //show info about image
             Console.WriteLine($"The CID of this node is: {node.Id} and it contains {node.Links.Count()} links to other files");
+
+            //Shows how CID are build
+            CIDBuilder builder = new CIDBuilder("images/index.jpg",HashingAlgorithm.SHA2_256);
+            Console.WriteLine("The generated CID is: " + builder.BuildCID());
+
+            Console.ReadLine();
         }
     }
 }
