6b4155810dccaea95ffba1eb63d5b2571f886a65
Michiel Schoofs <michiel.schoofs@student.hogent.be>
2020-05-15 15:05:31 +0200

    [feat] displayed image

--- Demo/Example app with ipfs/Image-of-the-day/src/components/App.js
+++ Demo/Example app with ipfs/Image-of-the-day/src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
 import './App.css';
 import ipfsClient from 'ipfs-http-client';
 
@@ -12,7 +11,8 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      buffer:null
+      buffer:null,
+      memehash:'QmPQZN62smuRxRACH8LGYHrNXQAKPbCEj8ciyagzYXmghR'
     };  
   }
 
@@ -33,17 +33,21 @@ class App extends Component {
   }
 
   //example hash: "QmPQZN62smuRxRACH8LGYHrNXQAKPbCEj8ciyagzYXmghR"
+  //example url: "https://ipfs.infura.io/ipfs/QmPQZN62smuRxRACH8LGYHrNXQAKPbCEj8ciyagzYXmghR"
   onSubmit = (event) => {
     event.preventDefault();
     console.log("setting to ipfs")
     //Add data to node, second argument is a callback function
     ipfs.add(this.state.buffer,(error,result)=>{
+      this.setState({memehash: result[0].hash});
       console.log('IPFS result',result);
       if(error){
         console.log('error',error)
         return;
       }
     })
+    //Step 2 is store file on the blockchain
+
   }
 
   render() {
@@ -68,7 +72,7 @@ class App extends Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={logo} className="App-logo" alt="logo" />
+                  <img src={`https://ipfs.infura.io/ipfs/${this.state.memehash}`} className="App-logo" alt="img"/>
                 </a>
                 <p>&nbsp;</p>
                 <h2>Change meme</h2>
